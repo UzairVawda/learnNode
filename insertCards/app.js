a4323e8e4fa0544595a71615779c637311d1febc
umv23 <umv23@cs.drexel.edu>
2020-02-15 16:15:52 -0500

    basic error validation

--- insertCards/app.js
+++ insertCards/app.js
@@ -4,13 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var hbs = require('express-handlebars');
+var expressSessions = require('express-session');
 
 var indexRouter = require('./routes/index');
 
 var app = express();
 
 // view engine setup
-app.engine('hbs', hbs({ extname: 'hbs', defaultLayout: 'Layout', layoutsDir: __dirname + '/views/layouts' }))
+app.engine('hbs', hbs({ extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts' }))
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressSessions({ secret: 'max', saveUninitialized: 'false', resave: 'false' }));
 
 app.use('/', indexRouter);
 
--- insertCards/routes/index.js
+++ insertCards/routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
+const { check, validationResult } = require('express-validator');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Welcome', condition: true, anyArray: ['cat', 'dog', 'fish'] });
+    res.render('index', { title: 'Form Validation', success: req.session.success, errors: req.session.errors });
+    req.session.errors = null;
 });
+
+router.post('/submit', [
+    //check
+    check('email').isEmail().withMessage('Invalid email address'),
+    check('password').isLength({ min: 4 }).withMessage('Minimum length password is 4 characters'),
+    check('password').isLength({ max: 15 }).withMessage('Maximum length password is 15 characters'),
+    check('password').equals('confirmPassword').withMessage('Password\'s do not match!'),
+
+], (req, res) => {
+    const errors = validationResult(req);
+    console.log(errors.mapped())
+    console.log(req.body);
+    res.render('index', { title: 'Form Validation', errors: errors.mapped() })
+
+});
+
 
 module.exports = router;
