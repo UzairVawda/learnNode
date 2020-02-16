9bafb7a12233b637aa003df88d40c7089e9dea15
umv23 <umv23@cs.drexel.edu>
2020-02-15 23:50:34 -0500

    added update and delete functionality

--- insertCards/routes/delete.js
+++ insertCards/routes/delete.js
@@ -1,13 +1,51 @@
 var express = require('express');
 var router = express.Router();
+var assert = require('assert');
+var url = "mongodb://localhost:27017";
+const MongoClient = require('mongodb').MongoClient;
+const MongoObject = require('mongodb').ObjectID;
+const client = new MongoClient(url);
+const dbName = 'test';
+
+var url = 'mongodb://localhost:27017/test';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('delete', { title: 'Delete' });
 });
 
-router.post('/delete', function(req, res, next) {
+router.get('/get-data', function(req, res, next) {
+    console.log("hit get data")
+    var resultArray = []
+    MongoClient.connect(url, function(err, client) {
+        assert.equal(null, err);
+        const db = client.db(dbName);
+        var cursor = db.collection('user-data').find();
+        cursor.forEach(function(doc, err) {
+            assert.equal(null, err);
+            resultArray.push(doc)
+        }, function() {
+            client.close();
+            res.render('delete', { title: 'Delete', items: resultArray })
+        });
+    });
+});
 
+router.post('/', function(req, res, next) {
+    console.log("hit delete on update")
+    var id = req.body.id;
+
+    MongoClient.connect(url, function(err, client) {
+        assert.equal(null, err);
+        const db = client.db(dbName);
+        db.collection('user-data').deleteOne({ "_id": MongoObject(id) }, function(error, result) {
+            assert.equal(null, error);
+            console.log("deleted")
+            client.close();
+        })
+    })
+
+    res.redirect("delete")
 });
 
 module.exports = router;
--- insertCards/routes/update.js
+++ insertCards/routes/update.js
@@ -1,13 +1,59 @@
 var express = require('express');
 var router = express.Router();
+var assert = require('assert');
+var url = "mongodb://localhost:27017";
+const MongoClient = require('mongodb').MongoClient;
+const MongoObject = require('mongodb').ObjectID;
+const client = new MongoClient(url);
+const dbName = 'test';
+
+var url = 'mongodb://localhost:27017/test';
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    console.log("hit update page")
     res.render('update', { title: 'Update' });
 });
 
-router.post('/update', function(req, res, next) {
+router.get('/get-data', function(req, res, next) {
+    console.log("hit get data from update")
+    var resultArray = []
+    MongoClient.connect(url, function(err, client) {
+        assert.equal(null, err);
+        const db = client.db(dbName);
+        var cursor = db.collection('user-data').find();
+        cursor.forEach(function(doc, err) {
+            assert.equal(null, err);
+            resultArray.push(doc)
+        }, function() {
+            client.close();
+            res.render('update', { title: 'Update', items: resultArray })
+        });
+    });
+});
 
+router.post('/', function(req, res, next) {
+    console.log("hit update on update")
+    var items = {
+        title: req.body.title,
+        author: req.body.author,
+        content: req.body.content
+    }
+
+    var id = req.body.id;
+
+    MongoClient.connect(url, function(err, client) {
+        assert.equal(null, err);
+        const db = client.db(dbName);
+        db.collection('user-data').updateOne({ "_id": MongoObject(id) }, { $set: items }, function(error, result) {
+            assert.equal(null, error);
+            console.log("updated")
+            client.close();
+        })
+    })
+
+    res.redirect("update")
 });
 
 module.exports = router;
