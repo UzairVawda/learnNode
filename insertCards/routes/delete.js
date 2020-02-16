c1116886554acac5b3ff24fdcb3fb86b5ea10988
umv23 <umv23@cs.drexel.edu>
2020-02-16 00:41:29 -0500

    added monk and refactored insert and delete, BUG in update

--- insertCards/routes/delete.js
+++ insertCards/routes/delete.js
@@ -1,13 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var assert = require('assert');
-var url = "mongodb://localhost:27017";
-const MongoClient = require('mongodb').MongoClient;
-const MongoObject = require('mongodb').ObjectID;
-const client = new MongoClient(url);
-const dbName = 'test';
-
-var url = 'mongodb://localhost:27017/test';
+var db = require('monk')('localhost:27017/test');
+var userData = db.get('user-data')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -15,36 +9,15 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/get-data', function(req, res, next) {
-    console.log("hit get data")
-    var resultArray = []
-    MongoClient.connect(url, function(err, client) {
-        assert.equal(null, err);
-        const db = client.db(dbName);
-        var cursor = db.collection('user-data').find();
-        cursor.forEach(function(doc, err) {
-            assert.equal(null, err);
-            resultArray.push(doc)
-        }, function() {
-            client.close();
-            res.render('delete', { title: 'Delete', items: resultArray })
-        });
+
+    userData.find({}).then((docs) => {
+        res.render('delete', { title: 'Delete', items: docs })
     });
 });
 
 router.post('/', function(req, res, next) {
-    console.log("hit delete on update")
     var id = req.body.id;
-
-    MongoClient.connect(url, function(err, client) {
-        assert.equal(null, err);
-        const db = client.db(dbName);
-        db.collection('user-data').deleteOne({ "_id": MongoObject(id) }, function(error, result) {
-            assert.equal(null, error);
-            console.log("deleted")
-            client.close();
-        })
-    })
-
+    userData.remove(id);
     res.redirect("delete")
 });
 
--- insertCards/routes/insert.js
+++ insertCards/routes/insert.js
@@ -1,55 +1,30 @@
 var express = require('express');
 var router = express.Router();
-var assert = require('assert');
-var url = "mongodb://localhost:27017";
-const MongoClient = require('mongodb').MongoClient;
-const client = new MongoClient(url);
-const dbName = 'test';
-
-var url = 'mongodb://localhost:27017/test';
+var db = require('monk')('localhost:27017/test');
+var userData = db.get('user-data')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('insert', { title: 'Insert' });
-    console.log("hit insert.js")
 });
 
 router.get('/get-data', function(req, res, next) {
-    console.log("hit get data")
-    var resultArray = []
-    MongoClient.connect(url, function(err, client) {
-        assert.equal(null, err);
-        const db = client.db(dbName);
-        var cursor = db.collection('user-data').find();
-        cursor.forEach(function(doc, err) {
-            assert.equal(null, err);
-            resultArray.push(doc)
-        }, function() {
-            client.close();
-            res.render('insert', { title: 'Insert', items: resultArray })
-        });
+    userData.find({}).then((docs) => {
+        res.render('insert', { title: 'Insert', items: docs })
     });
 });
 
 router.post('/', function(req, res, next) {
-    console.log("hit insert")
-    var items = {
+    var item = {
         title: req.body.title,
         author: req.body.author,
         content: req.body.content
-    }
+    };
 
-    MongoClient.connect(url, function(err, client) {
-        assert.equal(null, err);
-        const db = client.db(dbName);
-        db.collection('user-data').insertOne(items, function(error, result) {
-            assert.equal(null, error);
-            console.log("inserted")
-            client.close();
-        })
-    })
-
-    res.redirect("insert")
+    var insertCheck = userData.insert(item);
+    insertCheck.then((docs) => {
+        res.redirect("insert");
+    });
 
 });
 
